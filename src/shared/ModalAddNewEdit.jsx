b29340c1075091b496cda426d6d282b78a408143
Hiep <vuhiep.tran95@gmail.com>
2018-10-17 15:21:56 +0700

    Test cors

--- src/shared/ModalAddNewEdit.jsx
+++ src/shared/ModalAddNewEdit.jsx
@@ -44,7 +44,7 @@ class ModalAddNewEdit extends Component {
     var form = new FormData(
       document.getElementById("editForm-" + this.props.user.username)
     );
-    await Axios.put(`${SERVER}users`, form)
+    await Axios.put(`${SERVER}test`, form)
       .then(res => {
         this.setState({
           message: DisplayMessage(res.status, res.statusText, res.data)
